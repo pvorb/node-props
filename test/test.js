02fd183577ef42490252202e0528f03019d41938
Paul Vorbach <paul@vorb.de>
2012-02-08 15:54:16 +0100

    Adapt test

--- test/test.js
+++ test/test.js
@@ -1,29 +1,20 @@
 var props = require('../props.js');
 
-var json =
-	'{\n\
-		"title": "How to use node.js",\n\
-		"date": "2011-08-09T17:56:00",\n\
-		"tags": [ "example", "node.js" ]\n\
-	}\n\
-\n\
-\n\
-Node.js is a new technology...';
-
+var json = '{ "title": "How to use node.js", "date": "2011-08-09T17:56:00",'
+  + ' "tags": [ "example", "node.js" ] }\n\n\nNode.js is a new technology...';
 console.log(props(json));
 
-var yaml =
-'title: How to use node.js\n\
-date: 2011-08-09 17:56:00\n\
-tags:\n\
-  - example\n\
-  - node.js\n\
-\n\
-\n\
-Node.js is a new technology...\n\
-\n\
-\n\
-another note';
+var yaml = 'title: How to use node.js\n'
+  + 'date: 2011-08-09 17:56:00\n'
+  + 'tags:\n'
+  + '  - example\n'
+  + '  - node.js\n'
+  + '\n'
+  + '\n'
+  + 'Node.js is a new technology...\n'
+  + '\n'
+  + '\n'
+  + 'another note';
 
 console.log(props(yaml));
 
